8a3eef1ce70ecd7f44a0b291b30c1544126464be
Márk Megyeri <megyko1234@gmail.com>
2022-10-24 16:27:17 +0200

    Nyilvantart.java létrehozása

---
+++ src/Nyilvantart.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Nyilvantarto implements Nev {
+    @Override
+    public ArrayList<String> getNames() {
+
+        String[] nevek = {"Béla", "Géza"} ;
+        ArrayList<String>nevLista =
+        new ArrayList<>(Arrays.asList(nevek));
+        return nevLista;
+     
+    
+};
+}
