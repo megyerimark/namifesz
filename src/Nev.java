b2b1b7830f17e33e0c764f3d22ce2f132e8ed5b3
Márk Megyeri <megyko1234@gmail.com>
2022-10-24 16:25:55 +0200

    Nev.java- névjegyzék hozzáadva

--- src/Nev.java
+++ src/Nev.java
@@ -1,3 +1,16 @@
+/*
+* File: Nev.java
+* Author: Megyeri Márk Máté
+* Copyright: 2022-10-24, Megyeri Márk Máté
+* Group: Szoft II/N
+* Date: 2022-10-24
+* Github: https://github.com/megyerimark
+* Licenc: GNU GPL
+*/
+ 
+
+
+
 import java.util.ArrayList;
 
 public interface Nev {
